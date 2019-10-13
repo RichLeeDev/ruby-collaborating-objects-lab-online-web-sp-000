22fb11a5f4b1daa9c1c30e34b512fb4f28b8caf2
Richard Lee <richlee81192@gmail.com>
2019-10-13 02:58:26 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,5 @@
 class Mp3_importer
- 
-  def import(list_of_filenames)
+ def import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
   end
- end
-  
 end 
